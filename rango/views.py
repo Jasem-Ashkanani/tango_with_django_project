e87a1dcfae9794438022ac4a1ca365307ff39716
Jasem <2961000A@student.gla.ac.uk>
2025-01-24 16:13:16 +0000

    Finished Week 2: Templates, static files, and media setup

--- rango/views.py
+++ rango/views.py
@@ -3,4 +3,5 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Rango says hello friend...!")
+    return render(request, 'rango/index.html')
+
