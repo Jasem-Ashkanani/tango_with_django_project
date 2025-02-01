dc0ecabb743aa85aa1e69f3665fa3a8da26e7655
Jasem <2961000A@student.gla.ac.uk>
2025-02-01 00:18:00 +0000

    TestChap4Passed: Updated(index.html, about.html, settings.py, url.py, views.py)

--- rango/urls.py
+++ rango/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from rango import views 
+from django.conf import settings
+from django.conf.urls.static import static
 
 app_name = 'rango'
 
@@ -7,3 +9,6 @@ urlpatterns = [
     path('', views.index,  name='index'),
     path('about/', views.about, name='about'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
